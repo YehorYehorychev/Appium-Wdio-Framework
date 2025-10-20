bc9ba18b3774292fe3df9b2ef2a36af9cac1e4f0
Yehor Yehorychev <egor57899@gmail.com>
2025-10-20 14:43:09 -0700

    E2E Add, save and verify note test has been written

--- wdio.conf.js
+++ wdio.conf.js
@@ -25,7 +25,7 @@ exports.config = {
     //
     specs: [
         // ToDo: define location for spec files here
-        './test/specs/**/android-native*.js'
+        './test/specs/android/*.js'
     ],
     // Patterns to exclude.
     exclude: [
