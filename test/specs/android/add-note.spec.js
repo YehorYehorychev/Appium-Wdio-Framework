0acd930bd834ca211da040d77ae091bdf2317325
Yehor Yehorychev <egor57899@gmail.com>
2025-10-20 15:04:52 -0700

    Delete Note test has been written

--- test/specs/android/add-note.spec.js
+++ test/specs/android/add-note.spec.js
@@ -1,12 +1,12 @@
 describe('Add Notes', () => {
-    it('Skip tutorial', async () => {
-        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/btn_start_skip"]')
+    it.skip('Skip tutorial', async () => {
+        await $('//android.widget.Button[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/btn_start_skip"]')
             .click();
 
         await expect($('//*[@text="Add note"]')).toBeDisplayed();
     });
 
-    it('add a note, save changes & verify note', async () => {
+    it.skip('add a note, save changes & verify note', async () => {
         await $('//*[@text="Add note"]').click();
         await $('//*[@text="Text"]').click();
         await expect($('//*[@text="Editing"]')).toBeDisplayed();
