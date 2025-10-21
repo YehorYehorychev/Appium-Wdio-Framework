73426b462daf7f567313b69e6b66a0acd95bd5fd
Yehor Yehorychev <egor57899@gmail.com>
2025-10-21 16:34:02 -0700

    iOS Elements test has been written

--- test/specs/ios/ios.spec.js
+++ test/specs/ios/ios.spec.js
@@ -1,5 +1,7 @@
-describe('Todo Item', () => {
-    it('Sample', async () => {
-
+describe('iOS Find Element', () => {
+    it('find element by accessibility id', async () => {
+        await $('~Alert Views').click();
+        await $('~Simple').click();
+        await expect(await driver.getAlertText()).toContain("A Short Title Is Best");
     });
 });
