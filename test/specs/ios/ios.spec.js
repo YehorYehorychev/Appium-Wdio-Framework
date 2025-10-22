83a3ae2a6b5ad63962a3df3451077e7606a9248c
Yehor Yehorychev <egor57899@gmail.com>
2025-10-22 16:27:28 -0700

    iOS xpath elements test has been added

--- test/specs/ios/ios.spec.js
+++ test/specs/ios/ios.spec.js
@@ -16,4 +16,23 @@ describe('iOS Find Element', () => {
             console.log(await element.getText());
         }
     });
+
+    it('find element by xpath', async () => {
+        // xpath - (//tagname[@attribute=value])
+
+        // await $('//XCUIElementTypeStaticText[@name="Alert Views"]').click();
+        // await $('//XCUIElementTypeStaticText[@label="Simple"]').click();
+
+        await $('//*[@name="Alert Views"]').click();
+        await $('//*[@label="Simple"]').click();
+        await expect(await driver.getAlertText()).toContain("A Short Title Is Best");
+    });
+
+    it('find element by class chain', async () => {
+        // const alertText = '**/XCUIElementTypeStaticText[`label == "Alert Views"`]';
+        const alertText = '**/XCUIElementTypeStaticText[`label CONTAINS "Alert"`]';
+        await $(`-ios class chain:${alertText}`).click();
+        await $('//*[@label="Simple"]').click();
+        await expect(await driver.getAlertText()).toContain("A Short Title Is Best");
+    });
 });
