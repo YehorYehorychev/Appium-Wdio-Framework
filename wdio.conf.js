b0c3879df49a9f1c780355d2eeb3224adf9153af
Yehor Yehorychev <egor57899@gmail.com>
2025-10-22 16:34:43 -0700

    iOS predicate string test has been added

--- wdio.conf.js
+++ wdio.conf.js
@@ -25,7 +25,7 @@ exports.config = {
     //
     specs: [
         // ToDo: define location for spec files here
-        './test/specs/ios/ios.spec.js'
+        './test/specs/ios/ios-findElements.spec.js'
     ],
     // Patterns to exclude.
     exclude: [
