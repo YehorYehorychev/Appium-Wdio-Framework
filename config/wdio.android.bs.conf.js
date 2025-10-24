7800a25de66e5dc2c15820dafa3f933e9b8f37d7
Yehor Yehorychev <egor57899@gmail.com>
2025-10-24 16:22:12 -0700

    BrowserStack config has been added

--- config/wdio.android.bs.conf.js
+++ config/wdio.android.bs.conf.js
@@ -2,37 +2,47 @@ require('dotenv').config();
 const path = require('path');
 const { config } = require('./wdio.shared.conf');
 
-//
 // ============
 // BrowserStack Credentials
 // ============
 config.user = process.env.BROWSERSTACK_USER;
 config.key = process.env.BROWSERSTACK_KEY;
 
+// =========
+// Test Specs
+// =========
 config.specs = [
     path.join(process.cwd(), './test/specs/android/add-note-screen.spec.js')
 ];
 
+// =========
+// Capabilities
+// =========
 config.capabilities = [
     {
-        'appium:platformName': 'Android',
+        platformName: 'Android',
         'appium:platformVersion': '12.0',
-        'appium:deviceName': 'Pixel_6',
+        'appium:deviceName': 'Google Pixel 6',
         'appium:automationName': 'UIAutomator2',
-        'appium:app': "bs://9605c7e00c359dc03b21e58accccb9929060ab98",
+        'appium:app': 'bs://9605c7e00c359dc03b21e58accccb9929060ab98',
         'appium:autoGrantPermissions': true,
+
+        'bstack:options': {
+            projectName: 'Appium WDIO Framework',
+            buildName: 'Android Automation Build',
+            sessionName: 'Add Note Test',
+            appiumVersion: '2.0.0',
+            deviceOrientation: 'portrait',
+            networkLogs: true,
+            debug: true,
+            local: false
+        }
     }
-]
+];
 
-config.services = [['appium', {
-    args: {
-        address: 'localhost',
-        port: 4723,
-        relaxedSecurity: true
-    },
-    logPath: './'
-}]];
-
-// Test runner services
+// =========
+// Services
+// =========
 config.services = ['browserstack'];
+
 exports.config = config;
